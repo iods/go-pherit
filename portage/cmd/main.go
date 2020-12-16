dc08171274f80d4986579fed3a3c2d30d2634a03
Rye Miller <millerrye17@gmail.com>
2020-12-15 20:09:09 -0700

    Update string, number, and boolean types in the portage package.

--- portage/cmd/main.go
+++ portage/cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
-import (
-	"github.com/thedarksociety/go-pherit/portage"
-)
+import "github.com/thedarksociety/go-pherit/portage"
 
 func main() {
-	portage.SlickStrings()
+	// portage.SlickTypes()
+	// portage.InadazeTypes()
+	// portage.SunsetTypes()
+	portage.DanziTypes()
 }
---
+++ portage/sunset.go
@@ -0,0 +1,17 @@
+package portage
+
+import (
+	"fmt"
+	"strings"
+)
+
+// SunsetTypes Exportable function for representing the various boolean
+//   operations available in Go.
+func SunsetTypes() {
+	title := "Sunset: Boolean Types in Go."
+	fmt.Println(strings.Title(title))
+}
+
+func sunsetBool() {
+
+}
