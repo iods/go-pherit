b163a9070dfbf7aee760abcb6c73c23be70956c0
Rye Miller <millerrye17@gmail.com>
2020-12-15 21:50:47 -0700

    Addition of introduction course exercises.

---
+++ solve/apples.go
@@ -0,0 +1,18 @@
+/*
+Output:
+I started with 10 apples.
+Some jerk ate 4 apples.
+There are 6 apples left.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	var originalCount = 10
+    var eatenCount = 4
+
+	fmt.Println("I started with", originalCount, "apples.")
+	fmt.Println("Some jerk ate", eatenCount, "apples.")
+	fmt.Println("There are", originalCount - eatenCount, "apples left.")
+}
