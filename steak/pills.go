d878068c7dc8c7aa1eb32f937ba4ba15aa6d87d0
Rye Miller <millerrye17@gmail.com>
2020-12-19 14:15:43 -0700

    Update the steak package with array examples.

--- steak/pills.go
+++ steak/pills.go
@@ -3,7 +3,44 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
+
+/*
+auJus Returns an example of using integers in an array.
+*/
+func auJus() {
+	var primes [5]int
+	primes[0] = 2
+	primes[1] = 3
+	fmt.Println(primes[0])
+	fmt.Println(primes[2])
+}
+
+/*
+rare Returns an example of using time values within an array.
+*/
+func rare() {
+	var dates [3]time.Time
+	dates[0] = time.Unix(12578940090, 0)
+	dates[1] = time.Unix(12448940090, 0)
+	dates[2] = time.Unix(12578955530, 0)
+	fmt.Println(dates[1])
+}
+
+/*
+mediumRare Returns an example of zero values in arrays by setting one of integer counters.
+*/
+func mediumRare() {
+	var counters [3]int
+	counters[0]++
+	fmt.Println(counters[0])
+	counters[0]++
+	counters[2]++
+	fmt.Println(counters[0])
+	fmt.Println(counters[1])
+	fmt.Println(counters[2])
+}
 
 /*
 main Arrays are like a pill box, each element has it's own index, or slot.
@@ -18,4 +55,8 @@ func main() {
 	notes[2] = "mi"
 	fmt.Println(notes[0])
 	fmt.Println(notes[1])
+	fmt.Println(notes[3])
+	auJus()
+	rare()
+	mediumRare()
 }
