d487906e5100dcc4f6df356d78edf772d9a5eac1
Rye Miller <millerrye17@gmail.com>
2020-12-19 19:00:19 -0700

    Update array functions to include loops and blank identifiers.

--- steak/pills.go
+++ steak/pills.go
@@ -15,6 +15,7 @@ func auJus() {
 	primes[1] = 3
 	fmt.Println(primes[0])
 	fmt.Println(primes[2])
+	fmt.Println(primes)
 }
 
 /*
@@ -25,7 +26,10 @@ func rare() {
 	dates[0] = time.Unix(12578940090, 0)
 	dates[1] = time.Unix(12448940090, 0)
 	dates[2] = time.Unix(12578955530, 0)
-	fmt.Println(dates[1])
+
+	for i := 0; i < 3; i ++ {
+		fmt.Println(i, dates[i])
+	}
 }
 
 /*
@@ -43,6 +47,39 @@ func mediumRare() {
 }
 
 /*
+medium Returns an example of checking the array length in a for loop (resist the panic!)
+*/
+func medium() {
+	var letters = [7]string{"a", "b", "c", "d", "e", "f", "g"}
+	fmt.Println(len(letters))
+
+	for i := 0; i < len(letters); i++ {
+		fmt.Println(i, letters[i])
+	}
+}
+
+/*
+mediumWell Returns an example of using for...range loops with arrays. Stupid simple amazing.
+*/
+func mediumWell() {
+	var words = [6]string{"alpha", "beta", "charlie", "delta", "echo", "foxtrot"}
+	for index, word := range words {
+		fmt.Println(index, word)
+	}
+}
+
+/*
+well Returns an example of using a blank identifier in a for...range loop.
+*/
+func well() {
+	var words = [6]string{"alpha", "beta", "charlie", "delta", "echo", "foxtrot"}
+	for index, _ := range words {
+		fmt.Println(index)
+	}
+}
+
+
+/*
 main Arrays are like a pill box, each element has it's own index, or slot.
 */
 func main() {
@@ -50,10 +87,16 @@ func main() {
 	fmt.Println(strings.Title(title))
 
 	var notes = [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
-	fmt.Println(notes[0])
+	index := 0
+	fmt.Println(index, notes[index])
 	fmt.Println(notes[1])
+	index = 2
+	fmt.Println(index, notes[index])
 	fmt.Println(notes[3])
 	auJus()
 	rare()
 	mediumRare()
+	medium()
+	mediumWell()
+	well()
 }
