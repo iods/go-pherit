5776cebd4dbe94673eb2528705366b22d6d80ec0
Rye Miller <millerrye17@gmail.com>
2020-12-20 14:25:59 -0700

    Addition of variadic functions exercise.

---
+++ solve/veryattic.go
@@ -0,0 +1,44 @@
+/*
+Exercise:
+Using a variadic function, generate the output:
+16
+7
+
+Code Magnets:
+for _, number
+:=
+))
+int
+...
+(
+)
+}
+sum(
+,
+import "fmt"
+{
+}
+{
+}
+sum(
+4
+var sum int = 0
+func main() {
+7
+1
+,
+package main
+func sum
+fmt.Println(
+))
+sum += number
+return sum
+int
+9
+2
+numbers
+range
+numbers
+fmt.Println(
+,
+*/
