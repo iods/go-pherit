3306057e42daa32328cd5cffa1531bc13ca39977
Rye Miller <millerrye17@gmail.com>
2020-12-21 09:28:17 -0700

    Addition of maps with loops exercise.

---
+++ solve/mapsloops.go
@@ -0,0 +1,41 @@
+/*
+Exercise: Code Magnets
+Produce the Output
+The gold medal's rank is 1
+The bronze medal's rank is 3
+The silver medal's rank is 2
+
+package main
+}
+"bronze": 3
+:=
+:=
+import "fmt"
+}
+"silver": 2
+,
+,
+func main() {
+}
+"gold": 1
+,
+,
+,
+ranks
+{
+{
+fmt.Printf(
+)
+"The %s medal's rank is %d\n"
+map
+range
+ranks
+for
+[string]
+int
+medal
+rank
+medal
+rank
+*/
+
