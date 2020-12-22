48ee8daf25de31caa1133d599b8ed82349946578
Rye Miller <millerrye17@gmail.com>
2020-12-21 21:48:44 -0700

    Update to struct exercise to include passing structs with pointers.

--- solve/struct.go
+++ solve/struct.go
@@ -18,6 +18,24 @@ type student struct {
 	grade float64
 }
 
+type car struct {
+	name string
+	topSpeed float64
+}
+
+type part struct {
+	description string
+	count int
+}
+
+func nitroBoost(c *car) {
+	c.topSpeed += 50
+}
+
+func doublePack(p *part) {
+	p.count *= 2
+}
+
 func printInfo(s student) {
 	fmt.Println("Name:", s.name)
 	fmt.Printf("Grade: %0.1f\n", s.grade)
@@ -40,4 +58,20 @@ func main() {
 	s.grade = 92.3
 
 	printInfo(s)
+	fmt.Println()
+	// new exercise
+
+	var mustang car
+	mustang.name = "Ford Mustang Cobra"
+	mustang.topSpeed = 225
+	nitroBoost(&mustang)
+	fmt.Println(mustang.name)
+	fmt.Println(mustang.topSpeed)
+
+	var fuses part
+	fuses.description = "Fuses for the car."
+	fuses.count = 5
+	doublePack(&fuses)
+	fmt.Println(fuses.description)
+	fmt.Println(fuses.count)
 }
