3dcabb8ae069ac1659269ab8c02c6a0c32a95de0
Rye Miller <millerrye17@gmail.com>
2020-12-21 18:46:46 -0700

    Update to struct exercise with defined types and functions.

--- solve/struct.go
+++ solve/struct.go
@@ -3,10 +3,25 @@ Exercise:
 Output:
 Name: Max
 Age: 5
+---------------
+Exercise: Code Magnets
+Output:
+Name: Alonzo Cole
+Grade: 92.3
 */
 package main
 
 import "fmt"
+
+type student struct {
+	name string
+	grade float64
+}
+
+func printInfo(s student) {
+	fmt.Println("Name:", s.name)
+	fmt.Printf("Grade: %0.1f\n", s.grade)
+}
 
 func main() {
 	var pet struct{
@@ -18,4 +33,11 @@ func main() {
 
 	fmt.Println("Name", pet.name)
 	fmt.Println("Age:", pet.age)
+	fmt.Println()
+
+	var s student
+	s.name = "Alonzo Cole"
+	s.grade = 92.3
+
+	printInfo(s)
 }
