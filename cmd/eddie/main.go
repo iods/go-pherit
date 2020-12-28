b58892063ae91e687b60f9ef8295e4b8e18b8757
Rye Miller <millerrye17@gmail.com>
2020-12-28 15:20:01 -0700

    Update to the scanDirectory function with error handling.

--- cmd/eddie/main.go
+++ cmd/eddie/main.go
@@ -4,26 +4,58 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 )
 
-func cursed() {
-	fmt.Println("Oh shit, I am stuck in an infinite loop.")
-	cursed()
+/*
+cursed Returns an example of recursion.
+*/
+func cursed(start int, end int) {
+	fmt.Printf("cursed(%d, %d) called.\n", start, end)
+	fmt.Println("Starting at:", start)
+	if start < end {
+		cursed(start + 1, end)
+	}
+	fmt.Printf("Returning from cursed(%d, %d) called.\n", start, end)
 }
 
-func main() {
-	files, err := ioutil.ReadDir("track")
+/*
+scanDirectory Scans and prints the contents of the directory path provided.
+*/
+func scanDirectory(path string) error {
+	fmt.Println("Scanning:", path)
+	files, err := ioutil.ReadDir(path) // get a slice with the dir contents
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Returning error from scanDirectory(\"%s\") call \n", path)
+		return err
 	}
 
 	for _, file := range files {
+
+		filePath := filepath.Join(path, file.Name())
+
 		if file.IsDir() {
-			fmt.Println("Directory:", file.Name())
+			err := scanDirectory(filePath)
+			if err != nil {
+				fmt.Printf("Returning error from scanDirectory(\"%s\") call \n", path)
+				return err
+			}
 		} else {
-			fmt.Println("Filename:", file.Name())
+			fmt.Println(filePath)
 		}
+
+		//fmt.Println()
+		//fmt.Printf("%s | is dir: %t | Size: %d | %s\n", file.Name(), file.IsDir(), file.Size(), file.Mode())
+		//fmt.Printf("%s\n", file.ModTime())
 	}
 
-	cursed()
+	return nil
+}
+
+func main() {
+	err := scanDirectory("track")
+	if err != nil {
+		log.Fatal(err)
+	}
+	panic("oh no, sugar we are going down.")
 }
