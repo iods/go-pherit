b784797cc4fbd6136594890ead592aa9a06a2d47
Rye Miller <millerrye17@gmail.com>
2020-12-29 00:10:09 -0700

    Addition of false claims printf exercise.

---
+++ test/exercises/types/print_false.go
@@ -0,0 +1,20 @@
+/*
+
+Exercise:
+
+1. Use Printf to print the expected output using a variable.
+
+Output:
+These are false claims.
+
+Notes:
+
+*/
+package main
+
+import "fmt"
+
+func main() {
+	tf := false
+	fmt.Printf("These are %t claims.\n", tf)
+}
