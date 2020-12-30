a6dd0c9ff5eed8f6d7465e4350becc4580fa4e9f
Rye Miller <millerrye17@gmail.com>
2020-12-30 16:48:29 -0700

    Addition of larger function and update of join test for third conditional.

--- pkg/prose/join_test.go
+++ pkg/prose/join_test.go
@@ -1,14 +1,25 @@
 package prose
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestOneElement(t *testing.T) {
+	list := []string{"apple"}
+	want := "apple"
+	got := JoinWithCommas(list)
+	if want != got {
+		t.Error(errorString(list, got, want))
+	}
+}
 
 func TestTwoElements(t *testing.T) {
 	list := []string{"apple", "orange"}
 	want := "apple and orange"
 	got := JoinWithCommas(list)
 	if got != want {
-		// t.Error("did not match expected value")
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+		t.Error(errorString(list, got, want))
 	}
 }
 
@@ -17,7 +28,10 @@ func TestThreeElements(t *testing.T) {
 	want := "orange, apple, and cherry"
 	got := JoinWithCommas(list)
 	if got != want {
-		// t.Error("did not match expected value")
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+		t.Error(errorString(list, got, want))
 	}
+}
+
+func errorString(list []string, got string, want string) string {
+	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
 }
