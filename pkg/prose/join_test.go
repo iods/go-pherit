a07e8623fae9bcb7a353389924e925498c68749b
Rye Miller <millerrye17@gmail.com>
2020-12-30 14:21:16 -0700

    Update to tests with expected output.

--- pkg/prose/join_test.go
+++ pkg/prose/join_test.go
@@ -3,9 +3,15 @@ package prose
 import "testing"
 
 func TestTwoElements(t *testing.T) {
-	t.Error("no test written yet")
+	list := []string{"orange", "apple"}
+	if JoinWithCommas(list) != "orange and apple" {
+		t.Error("did not match expected value")
+	}
 }
 
 func TestThreeElements(t *testing.T) {
-	t.Error("no test written here either")
+	list := []string{"orange", "apple", "cherry"}
+	if JoinWithCommas(list) != "orange, apple, and cherry" {
+		t.Error("did not match the expected value")
+	}
 }
