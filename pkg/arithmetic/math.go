4333adc5e881c3102802dc194b9529461b7f4514
Rye Miller <millerrye17@gmail.com>
2020-12-31 01:31:10 -0700

    Update to include negate and multiply functions and tests.

--- pkg/arithmetic/math.go
+++ pkg/arithmetic/math.go
@@ -1,15 +1,26 @@
 package arithmetic
 
 /*
-Add Returns the sum of two numbers for testing tests.
-*/
+Add Returns the sum of two numbers for testing tests. */
 func Add(a float64, b float64) float64 {
 	return a + b
 }
 
 /*
-Subtract Returns the difference of two numbers for testing tests.
-*/
-func Subtract(x float64, y float64) float64 {
-	return x - y
+Subtract Returns the difference of two numbers for testing tests. */
+func Subtract(a float64, b float64) float64 {
+	return a - b
+}
+
+/*
+Multiply Returns the product (*) of two numbers. */
+func Multiply(a float64, b float64) float64 {
+	return a * b
+}
+
+/*
+Negate Returns the expression with it's sign changed.
+ */
+func Negate(a float64) float64 {
+	return -(a)
 }
--- test/arithmetic/math_test.go
+++ test/arithmetic/math_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	if arithmetic.Add(1, 2) != 5 {
+	if arithmetic.Add(1, 2) != 3 {
 		t.Error("1 + 2 did not equal 3")
 	}
 }
@@ -14,5 +14,17 @@ func TestAdd(t *testing.T) {
 func TestSubtract(t *testing.T) {
 	if arithmetic.Subtract(2, 1) != 1 {
 		t.Error("2 - 1 did not equal 1")
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	if arithmetic.Multiply(2, 3) != 6 {
+		t.Error("2 * 3 did not equal 6")
+	}
+}
+
+func TestNegate(t *testing.T) {
+	if arithmetic.Negate(-2) != 2 {
+		t.Error("-2 did not turn into a positive 2")
 	}
 }
