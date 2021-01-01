0753254564271edcf700a773031e94ac7de2de66
Rye Miller <millerrye17@gmail.com>
2020-12-31 21:47:20 -0700

    Addition of fix-the-floats exercise to numbers.

---
+++ examples/exercises/types/numbers/floats.go
@@ -0,0 +1,25 @@
+/*
+Go-Pherit: A Learning Experience and personal SDK in Golang
+Copyright (c) 2020 Rye Miller
+https://darksociety.io
+*/
+
+package main
+
+/*
+Exercise: Fix the Float
+1. Fix the program to print 2.5 instead of 2
+
+Output:
+2.5
+
+Notes:
+
+*/
+
+import "fmt"
+
+func main() {
+	x := 5. / 2
+	fmt.Println(x)
+}
