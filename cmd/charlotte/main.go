4fa17e8edd097057bc0ab4434e59484cbb7786cd
Rye Miller <millerrye17@gmail.com>
2021-01-14 12:24:20 -0700

    Update to the iods namespace.

--- cmd/charlotte/main.go
+++ cmd/charlotte/main.go
@@ -1,53 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
 
-func write(writer http.ResponseWriter, message string) {
-	_, err := writer.Write([]byte(message))
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-/*
-indexHandler Function to manage the index route. http://localhost:8150/ */
-func IndexHandler(response http.ResponseWriter, request *http.Request, _ httprouter.Params) {
-	write(response, "Charlotte homepage.")
-}
-
-// englishHandler
-func englishHandler(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
-	write(writer, "Hello, Charlotte!")
-}
-
-func frenchHandler(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
-	write(writer, "Bonjour, Charlotte!")
-}
-
-func germanHandler(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
-	write(writer, "Guten Tag, Charlotte!")
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintf(w, "Thanks, %s!\n", p.ByName("name"))
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, World. It's Charlotte.")
 }
 
 func main() {
-
-	r := httprouter.New()
-
-	r.GET("/", IndexHandler)
-	r.GET("/english", englishHandler)
-	r.GET("/french", frenchHandler)
-	r.GET("/german", germanHandler)
-	r.GET("/charlotte/:name", postHandler)
-
-	err := http.ListenAndServe("localhost:9111", r)
-	log.Fatal(err)
+	http.HandleFunc("/hello", HelloHandler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
--- pkg/prose/larger.go
+++ pkg/prose/larger.go
@@ -1,8 +1,6 @@
 package prose
 
-/*
-Larger Return the larger integer for testing tests and helpers.
-*/
+// Larger Return the larger integer for testing tests and helpers.
 func Larger(a int, b int) int {
 	if a > b {
 		return a
